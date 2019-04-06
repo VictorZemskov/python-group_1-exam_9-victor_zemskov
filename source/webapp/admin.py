c87607e5d844c9811898dc6eaaa0c45b198dcfd3
Victor <vzemskov090583@gmail.com>
2019-04-06 13:17:11 +0600

    Added models Product, ProductPhoto, Category, Order

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from webapp.models import Product, ProductPhoto, Category, Order
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'date_of_entry']
+    ordering = ['-date_of_entry']
+    search_fields = ['name', 'id']
+
+class ProductPhotoAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'photo', 'product']
+    ordering = ['pk']
+    search_fields = ['photo', 'id']
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name']
+    ordering = ['pk']
+    search_fields = ['name', 'id']
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'user', 'product']
+    ordering = ['pk']
+    search_fields = ['id']
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductPhoto, ProductPhotoAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Order, OrderAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.urls import reverse
+import random
+import string
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
-# Create your models here.
+class SoftDeleteManager(models.Manager):
+    def active(self):
+        return self.filter(is_deleted=False)
+
+    def deleted(self):
+        return self.filter(is_deleted=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    date_of_entry = models.DateField()
+    category = models.ManyToManyField('Category', blank=True, related_name='categories')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_deleted = models.BooleanField(default=False)
+
+    objects = SoftDeleteManager()
+
+    def get_absolute_url(self):
+        return reverse('api_v1:product-detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.name
+
+class ProductPhoto(models.Model):
+    product = models.ForeignKey('Product', related_name='product_photos', on_delete=models.PROTECT)
+    photo = models.ImageField(upload_to='photos')
+    is_deleted = models.BooleanField(default=False)
+
+    objects = SoftDeleteManager()
+
+    def get_absolute_url(self):
+        return reverse('api_v1:product_photo-detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.photo
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    is_deleted = models.BooleanField(default=False)
+
+    objects = SoftDeleteManager()
+
+    def get_absolute_url(self):
+        return reverse('api_v1:category-detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    user = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', related_name='orders', on_delete=models.PROTECT)
+    tel_num = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    comment = models.TextField(max_length=2000, null=True, blank=True)
+    creation_date = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    objects = SoftDeleteManager()
+
+    def get_absolute_url(self):
+        return reverse('api_v1:order-detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return 'Order %s User %s' % (self.pk, self.user)
